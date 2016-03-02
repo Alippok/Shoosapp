8b918cbde8bab7c04a351b4db3cd9b0a09e2a094
A_Lippok <alippok@hotmail.co.uk>
2016-03-02 16:51:57 +0000

    Checks shoe_order class is responding

--- specs/shoe_order_spec.rb
+++ specs/shoe_order_spec.rb
@@ -5,10 +5,26 @@ require_relative('../models/shoe_order.rb')
 class TestShoeOrder < MiniTest::Test
 
   def setup
-    @shoe_order = Shoe_order.new
+    params = {
+      "first_name" => "Johnny",
+      "last_name" => "Bloggs",
+      "first_address_line" => "23 Crag Street",
+      "city" => "Edinburgh",
+      "postcode" => "EH3 12TY",
+      "quantity" => "2",
+      "size" => "9"
+    }
+    @shoe_order = Shoe_order.new(params)
 
   end
 
+  def test_first_name
+    assert_equal("Johnny", @shoe_order.first_name)
+  end
+
+  def test_second_name
+    assert_equal("Bloggs", @shoe_order.last_name)
+  end
 
 
 
