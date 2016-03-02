81841948c5ddc166752bf48df433bd704d2f5f73
A_Lippok <alippok@hotmail.co.uk>
2016-03-02 19:50:36 +0000

    Adds some styling to present summary

--- controller.rb
+++ controller.rb
@@ -5,3 +5,12 @@ require_relative('./models/shoe_order.rb')
 get '/shoe/new' do
   erb(:new)
 end
+
+post '/shoe' do
+  @shoe_order = Shoe_order.new(params)
+  @shoe_order.save()
+  erb(:summary)
+end
+
+
+
